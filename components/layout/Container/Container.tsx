22898c7402e5dad6539b19f4151ff25852ce5a38
Raymond Kneipp <raymond.f.kneipp@gmail.com>
2022-12-31 15:56:37 -0500

    fresh t3 install

--- components/layout/Container/Container.tsx
+++
@@ -1,7 +0,0 @@
-import { NextPage } from 'next';
-
-const Container: NextPage = ({ children }) => (
-  <div className="container mx-auto px-2">{children}</div>
-);
-
-export default Container;
--- components/list/List/List.tsx
+++
@@ -1,9 +0,0 @@
-import { NextPage } from 'next';
-import { IconType } from 'react-icons';
-import { Typography } from '../..';
-
-const List: NextPage = ({ children }) => (
-  <ul className="space-y-4">{children}</ul>
-);
-
-export default List;
