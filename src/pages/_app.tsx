e7715397b71a78a8ce66002a69b8a4bd70b094f0
Raymond Kneipp <raymond.f.kneipp@gmail.com>
2022-12-31 16:28:41 -0500

    add body and heading fonts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,17 @@
-import { type AppType } from "next/app";
+import { type AppType } from 'next/app'
+import { trpc } from '../utils/trpc'
+import '../styles/globals.css'
+import { Inter, Syne } from '@next/font/google'
 
-import { trpc } from "../utils/trpc";
-
-import "../styles/globals.css";
+const inter = Inter({ subsets: ['latin'], variable: '--font-inter' })
+const syne = Syne({ subsets: ['latin'], variable: '--font-syne', weight: '700' })
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
-};
+  return (
+    <main className={`${inter.variable} ${syne.variable} font-sans`}>
+      <Component {...pageProps} />
+    </main>
+  )
+}
 
-export default trpc.withTRPC(MyApp);
+export default trpc.withTRPC(MyApp)
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,15 @@
+const { fontFamily } = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['var(--font-inter)', ...fontFamily.sans],
+        heading: ['var(--font-syne)', ...fontFamily.serif],
+      },
+    },
   },
   plugins: [],
-};
+}
