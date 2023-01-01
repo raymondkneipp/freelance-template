803f8ef8420197d1f7cade23e2f8fe9a6a45a65c
Raymond Kneipp <raymond.f.kneipp@gmail.com>
2022-12-31 23:01:56 -0500

    create footer component

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,11 @@
+import { Container, Text } from '$components'
+
+export const Footer: React.FC = () => {
+  return (
+    <footer className="border-t py-8">
+      <Container>
+        <Text variant='h6'>Footer</Text>
+      </Container>
+    </footer>
+  )
+}
